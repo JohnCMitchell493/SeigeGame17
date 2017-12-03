c488e557af85db8be6de21ff46089c4ce78b7e4d
JohnCMitchell493 <33102502+JohnCMitchell493@users.noreply.github.com>
2017-12-03 17:28:47 -0500

    Updated Castle.py
    >added import pygame
    >self.image uses pygame.image.load() to load filename
    >castle is now a sprite, not a surface

--- castle.py
+++ castle.py
@@ -1,12 +1,14 @@
+import pygame
 class castle:
 	hp = 3
 	def __init__(self, name, filename):
 		self.name = name
 		self.x = xcoor
 		self.y = ycoor
-		self.image = filename
+		self.image = pygame.image.load(filename)
 #		location = random.randrange(100,380)		#Chooses xcoordinate for castle's random location
-		castle = pygame.rect(350,250,10,10)		#creates castle rect
+#		castle = pygame.rect(350,250,10,10)		#creates castle rect
+		castle = pygame.sprite.Sprite(self.file,75,75)
 
 	def getHit(self):
 		hp -= 1		#if the castle gets hit, it loses 1 HP.  The game is won if the castle is struck 3 times
