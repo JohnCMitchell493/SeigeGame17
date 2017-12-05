be78eb0bac8ed8f53b8d77f233d0859d917c38c6
XavierVGarcia <33102276+XavierVGarcia@users.noreply.github.com>
2017-12-05 14:58:20 -0500

    changed cmath and cmath instances to math

--- rock.py
+++ rock.py
@@ -1,6 +1,6 @@
 import pygame
-import cmath
 import castle
+import math
 
 class Rock(pygame.sprite.Sprite):
 
@@ -26,21 +26,21 @@ class Rock(pygame.sprite.Sprite):
 		self.velocity = velocity
 		gravity = 9.8		#gravity is constant
 		self.time = 10000
-		velx = (velocity * cmath.sin(angle))/(cmath.sin(90))	#calculates initial x distance
-		vely = (velocity * cmath.sin(180-(angle+90)))/(cmath.sin(90))	#calculates initial y distance
+		velx = (velocity * math.sin(angle))/(math.sin(90))	#calculates initial x distance
+		vely = (velocity * math.sin(180-(angle+90)))/(math.sin(90))	#calculates initial y distance
 #		xcoor = velx/i
 #		ycoor = vely/i
 		#ball.pygame.Rect.move(velx, vely)	#moves to coordinates calculated above.  This is also the initial velocity for the fromula loop below
 		for i in range(self.time):		#formula loop.  i is time.  Does not actually loop every second, look into later
 #			newvely = vely - (gravity*i)
 			xmove = velx*i					#horizontal distance = speed*time, horizontal speed is constant because no wind
-			ymove = 480-((vely)(i)-(.5)(gravity)((i)**2))		#vertical distance = (initial velocity)(time) - (1/2)(gravity)(time**2)
+			ymove = 480-((vely*i)-(.5*gravity*(i**2)))		#vertical distance = (initial velocity)(time) - (1/2)(gravity)(time**2)
 			self.rect.move(xmove,ymove)		#Move to new location based on calculated position
 #			vely = newvely
-			if checkCollision(self.rect, castle.rect) == False:
-				if xmove >= 400:				#if the rock's position moves to below the 'ground', or to beyond the 'right
-					self.rect.move(500,500)		#border', the rock automatically relocates to a position outside the game
+			if Rock.checkCollision(self.rect, castle.rect) == False:
+				if xmove >= 640:				#if the rock's position moves to below the 'ground', or to beyond the 'right
+					self.rect.move(700,700)		#border', the rock automatically relocates to a position outside the game
 					return 'Miss!'
-				if ymove >= 300:
-					self.rect.move(500,500)
+				if ymove >= 480:
+					self.rect.move(700,700)
 					return 'Miss!'
