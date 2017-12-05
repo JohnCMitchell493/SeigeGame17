297c1e255f570710c0446e550b05d5624ce931b1
XavierVGarcia <33102276+XavierVGarcia@users.noreply.github.com>
2017-12-05 01:39:23 -0500

    Commented the logic for victory/retry screen

--- MainLoop.py
+++ MainLoop.py
@@ -18,40 +18,34 @@ class Controller:
 		self.textfont = pygame.font.SysFont("helvetica", 15)
 		self.startRect = pygame.Rect(180,320,80,30)
 		self.quitRect = pygame.Rect(380,320,80,30)
-
+		self.cannon = cannon.Cannon("Cannon","cannon.jpg")
+		self.castle = castle.Castle("Castle","castle.png")
+		#self.sprites = pygame.sprite.Group((self.cannon)) #Used for drawing the sprites, still a work in progress 
 	#Start Button
 	def startButton(self):
-		start = pygame.draw.rect(self.screen, (192,192,192), self.startRect, 0)
-		#Draws the rectangle
+		start = pygame.draw.rect(self.screen, (192,192,192), self.startRect, 0)#Draws start the rectangle
 		startGame = self.textfont.render("Start", 1, (255, 255, 0))
-		self.screen.blit(startGame, (150, 200))	
-		#Blits the text/button to the rect
-		pygame.display.update()
-		#Updates the screen
+		self.screen.blit(startGame, (205, 325))	#Blits the text/button to the rect
+
 
 
 	#Quit Button
 	def quitButton(self):
-		quit = pygame.draw.rect(self.screen, (192,192,192), self.quitRect, 0)
-		#Draws the rectangle
+		quit = pygame.draw.rect(self.screen, (192,192,192), self.quitRect, 0)#Draws quit the rectangle
 		quitGame = self.textfont.render("Quit", 1, (255, 255, 0))
-		self.screen.blit(quitGame, (250, 200))	
-		#Blits the text/button to the rect
-		pygame.display.update()
-		#Updates the screen
+		self.screen.blit(quitGame, (410, 325))	#Blits the text/button to the rect
+
 
 		
-	def mainGame(self):
-		#Main Game screen
-		self.cannon = cannon.Cannon("Cannon","cannon.jpg")
-		self.castle = castle.Castle("Castle","castle.png")
-		pygame.display.update()
-		#At some point check castle's health and if it == 0, change to victory screen
+	#def mainGame(self): #Main Game screen
+		
+#		self.screen.blit(cannon.Cannon("Cannon", "cannon.jpg"),(0,0)) #Might not be necessary
+#		self.screen.blit(self.cannon(angle_label, (200,200))) 
+#		self.sprites.draw(self.screen) draws the sprites the the screen(work in progress)
+
 
 	def mainLoop(self):
-		self.startButton()
-		self.quitButton()
-		#display title screen with start and quit options
+
 
 		#Event Processing
 		done = False
@@ -59,8 +53,15 @@ class Controller:
 		title = False
 		while not done:
 			titleScreen = pygame.image.load("background.jpg")
+			self.startButton()#display title screen with start and quit options
+			self.quitButton()
 			if title == True:
 				self.screen.fill((0,225,225))
+#			if "insert castle syntax here" hp == 0:
+#				title = False #stops the screen from being blitted blue
+#				start = False #Allows 'q' to quit again
+#				"import victory background for x amount of seconds"
+#				put retry screen
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					done = True
@@ -79,31 +80,30 @@ class Controller:
 						self.cannon.powerChange(power)
 					if event.key == pygame.K_SPACE:
 						self.cannon.shoot()
-					if event.key == pygame.K_s:
-					    start = True
-					    #start == FALSE: will set this to true when game starts to prevent reloading the game
-					    title = True
-					    self.mainGame()
+					if event.key == pygame.K_s or pygame.K_r:
+						start = True #start == FALSE: will set this to true when game starts to prevent reloading the game
+						title = True
+						#self.mainGame()
 					if event.key == pygame.K_q: 
 						if start == False:
 						    done = True
 						    pygame.quit()
 						    sys.exit()
-					#if event.key == pygame.MOUSEBUTTONDOWN:
-						#mouse = pygame.mouse.get_pos()
-						#print(mouse)
-						#if quit.collide(mouse):
-							#stuff that would happen when 
-							#the mouse hits the start button
+#					if event.key == pygame.MOUSEBUTTONDOWN:
+#						mouse = pygame.mouse.get_pos()
+#						print(mouse)
+#						if quit.collide(mouse):
+#							stuff that would happen when 
+#							the mouse hits the start button
 			pygame.display.update()
 			self.screen.blit(self.background, (0,0))
 			self.screen.blit(titleScreen,(0,0))
 			
 
-			#Victory Screen 
+ 
 
 
-			#Post Victory screen with quit and retry options
+
 
 def main():
 	main_window = Controller()
