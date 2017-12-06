da6a12d104162ba38a679cbb56e143ff8fc63b5c
XavierVGarcia <33102276+XavierVGarcia@users.noreply.github.com>
2017-12-06 16:28:02 -0500

    updated get hit method

--- castle.py
+++ castle.py
@@ -1,24 +1,25 @@
 import pygame
 import math
 class Castle(pygame.sprite.Sprite):
-	hp = 3
 	def __init__(self, xcoor, ycoor, filename):
 		pygame.sprite.Sprite.__init__(self)
 #		self.x = xcoor
 #		self.y = ycoor
 		self.image = pygame.image.load(filename).convert()
 		self.size = self.image.get_size()
-		self.image2 = pygame.transform.scale(self.image, (int(self.size[0]*.1), int(self.size[1]*.1)))
-		self.rect = self.image2.get_rect()
+		self.rect = self.image.get_rect()
 		self.rect.x = xcoor
 		self.rect.y = ycoor
+		self.hp = 3
 #		location = random.randrange(100,380)		#Chooses xcoordinate for castle's random location
 
 
 	def getHit(self):
-		hp -= 1		#if the castle gets hit, it loses 1 HP.  The game is won if the castle is struck 3 times
+		self.hp -= 1		#if the castle gets hit, it loses 1 HP.  The game is won if the castle is struck 3 times
+		if self.hp < 0:
+			self.hp += 1
 #		return hp
 #		print("Hit!")
 
 	def resetHP(self):
-		hp = 3		#bug testing, in case we need to run it again
+		self.hp = 3		#bug testing, in case we need to run it again
