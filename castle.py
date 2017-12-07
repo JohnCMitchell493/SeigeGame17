08f5bb39bfec38ae6859ec5fd83a2af75975ef46
XavierVGarcia <33102276+XavierVGarcia@users.noreply.github.com>
2017-12-06 23:56:43 -0500

    Finalized castle.py

--- castle.py
+++ castle.py
@@ -2,6 +2,11 @@ import pygame
 import math
 class Castle(pygame.sprite.Sprite):
 	def __init__(self, xcoor, ycoor, filename):
+		'''
+		Description: Initializes the Castle class
+		Parameters: self, xcoor, ycoor, filename - self is the default class parameter, xcoor and ycoor are the passed in coordinates, filename is the file's name
+		Return: none
+		'''
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(filename).convert()
 		self.size = self.image.get_size()
@@ -9,15 +14,22 @@ class Castle(pygame.sprite.Sprite):
 		self.rect.x = xcoor
 		self.rect.y = ycoor
 		self.hp = 3
-#		location = random.randrange(100,380)		#Chooses xcoordinate for castle's random location
 
 
-	def getHit(self):
-		self.hp -= 1		#if the castle gets hit, it loses 1 HP.  The game is won if the castle is struck 3 times
-		if self.hp < 0:
+	def getHit(self): #Controls the castle's hp when hit
+		'''
+		Description: Lowers the castle's hp by one every time it gets hit
+		Parameters: self - self is the default class parameter
+		Return: none
+		'''
+		self.hp -= 1	#if the castle gets hit, it loses 1 HP.  
+		if self.hp < 0: 	#prevents the hp from going lower than 0
 			self.hp += 1
-#		return hp
-#		print("Hit!")
 
-	def resetHP(self):
-		self.hp = 3		#bug testing, in case we need to run it again
+	def resetHP(self): #reset's castle's hp
+		'''
+		Description: Sets the castle's hp to 3
+		Parameters: self - self is the default class paramter
+		Return: none
+		'''
+		self.hp = 3		#sets the castle's hp back to 3
